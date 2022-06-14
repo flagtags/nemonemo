986723921f30462dd35e2df20adc310028e4d070
Dante <81557077+dante022@users.noreply.github.com>
2022-06-14 21:50:35 +0900

    유저, 유저 엔티티 모듈 추가

--- packages/server/src/app.module.ts
+++ packages/server/src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { User.EntityService } from './user.entity/user.entity.service';
+import { UserEntityService } from 'src/user/user-entity.service';
+import { User.EntityService } from './user.entity/user.entity.service';
 
 @Module({
-  imports: [],
+  imports: [UserModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, User.EntityService, UserEntityService],
 })
 export class AppModule {}
