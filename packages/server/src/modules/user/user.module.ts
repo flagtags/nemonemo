48b82ef0160a10fc01d89adc0308094148c9d688
박재윤 <j03y14@naver.com>
2022-06-21 22:39:32 +0900

    user 모델, 모듈 생성 (#5)

--- packages/server/src/modules/user/user.module.ts
+++ packages/server/src/modules/user/user.module.ts
@@ -1,11 +1,15 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UserEntityService } from '@entities/user-entity/user-entity.service';
 import { UserController } from '@controllers/user/user.controller';
 import { UserService } from '@use-cases/user/user.service';
-import { UserEntityService } from '@entities/user-entity/user-entity.service';
+import { User, UserSchema } from '@models/user/user.schema';
 
 @Module({
-    imports: [],
-    controllers: [UserController],
-    providers: [UserService, UserEntityService],
+  imports: [
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+  ],
+  controllers: [UserController],
+  providers: [UserService, UserEntityService],
 })
 export class UserModule {}
