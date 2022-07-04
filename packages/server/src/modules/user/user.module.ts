a91b8a0e3d263e8d22faa64d7bed7d03e92d36ad
박재윤 <j03y14@naver.com>
2022-07-04 23:21:53 +0900

    유저 모듈 provider에 UserModel 추가 (#5)

--- packages/server/src/modules/user/user.module.ts
+++ packages/server/src/modules/user/user.module.ts
@@ -4,12 +4,13 @@ import { UserEntityService } from '@entities/user-entity/user-entity.service';
 import { UserController } from '@controllers/user/user.controller';
 import { UserService } from '@use-cases/user/user.service';
 import { User, UserSchema } from '@models/user/user.schema';
+import { UserModel } from '@models/user/user.model';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
   ],
   controllers: [UserController],
-  providers: [UserService, UserEntityService],
+  providers: [UserService, UserEntityService, UserModel],
 })
 export class UserModule {}
