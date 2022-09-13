a51c8b2b02f3424a29120f6ed7ab2005ccebc79a
Dante <48055710+kkirico@users.noreply.github.com>
2022-09-13 21:53:49 +0900

    user not found 에러 추가 #5

--- packages/server/src/errors/user.ts
+++ packages/server/src/errors/user.ts
@@ -1,5 +1,6 @@
 enum UserError {
   DUPLIACED_USER = 202,
+  USER_NOT_FOUND = 203,
 }
 
 export class DuplicatedUserError extends Error {
@@ -9,5 +10,15 @@ export class DuplicatedUserError extends Error {
     super('Duplicated User');
 
     this.code = UserError.DUPLIACED_USER;
+  }
+}
+
+export class UserNotFoundError extends Error {
+  code: number;
+  
+  constructor() {
+    super('User Not Found');
+
+    this.code = UserError.USER_NOT_FOUND;
   }
 }
