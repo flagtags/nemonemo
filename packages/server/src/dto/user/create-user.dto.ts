d11f3fa50f1308a79b3d911aaf0f0b1b68cf7a28
박재윤[Travel & Finance Service Dev] <j03y14@naver.com>
2023-01-30 22:44:41 +0900

    CreateUserDto validation 추가 #5
    Co-authored-by: Heon-Jun Lee <48055710+kkirico@users.noreply.github.com>

--- packages/server/src/dto/user/create-user.dto.ts
+++ packages/server/src/dto/user/create-user.dto.ts
@@ -1,5 +1,25 @@
+import {
+  IsBoolean,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  IsStrongPassword,
+} from 'class-validator';
 export class CreateUserDto {
+  @IsNotEmpty()
+  @IsString()
   readonly name: string;
+
+  @IsNotEmpty()
+  @IsString()
   readonly userName: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @IsStrongPassword()
   readonly password: string;
+
+  @IsOptional()
+  @IsBoolean()
+  readonly isBanned?: boolean = false;
 }
