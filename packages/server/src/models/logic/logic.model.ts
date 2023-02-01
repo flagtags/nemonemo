6e92b264d6b03b33f88f237e099e5f543013c3a3
Dante <48055710+kkirico@users.noreply.github.com>
2023-02-01 21:31:05 +0900

    로직 모델 추가 #8

---
+++ packages/server/src/models/logic/logic.model.ts
@@ -0,0 +1,53 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { filterEmptyObjectField } from '@utils/index';
+import { FindLogicsDto } from '@dto/logic/find-logics.dto';
+import { FindOneLogicDto } from '@dto/logic/find-one-logic.dto';
+import { UpdateLogicDto } from '@dto/logic/update-logic.dto';
+import { CreateLogicDto } from '@dto/logic/create-logic.dto';
+import { DeleteLogicDto } from '@dto/logic/delete-logic.dto';
+import { Logic, LogicDocmuent } from './logic.schema';
+
+@Injectable()
+export class LogicModel {
+  constructor(@InjectModel(Logic.name) private logicSchema: Model<Logic>) {}
+
+  findLogics(findLogicDto: FindLogicsDto): Promise<LogicDocmuent[]> {
+    const { pageSize, pageIndex } = findLogicDto;
+    const skipNum = pageSize * (pageIndex - 1);
+
+    return this.logicSchema.find().limit(pageSize).skip(skipNum).exec();
+  }
+
+  findOneLogic(findOneLogicDto: FindOneLogicDto): Promise<LogicDocmuent> {
+    return this.logicSchema.findOne({ id: findOneLogicDto.id }).exec();
+  }
+
+  createLogic(createLogicDto: CreateLogicDto): Promise<LogicDocmuent> {
+    const logicDocument = new this.logicSchema(createLogicDto);
+    const logic = logicDocument.save();
+
+    return logic;
+  }
+
+  async updateLogic(updateLogicDto: UpdateLogicDto): Promise<boolean> {
+    const { _id, ...restUpdatedLogicDto } = updateLogicDto;
+
+    const filteredRestUpdateLogicDto =
+      filterEmptyObjectField(restUpdatedLogicDto);
+    return !!(await this.logicSchema.updateOne(
+      { _id: updateLogicDto._id },
+      { $set: filteredRestUpdateLogicDto },
+    ));
+  }
+
+  async deleteLogic(removeLogicDto: DeleteLogicDto): Promise<boolean> {
+    const res = await this.logicSchema
+      .deleteOne({ _id: removeLogicDto._id })
+      .exec();
+
+    if (res.deletedCount === 0) throw new Error('logic not found');
+    return true;
+  }
+}
