5f79fcf259b52d8ac4f5ca9c5bd08dd03f620d1c
박재윤[Travel & Finance Service Dev] <j03y14@naver.com>
2023-03-01 23:32:29 +0900

    getHints logic entity에서 제거 #8
    Co-Authored-By: Heon-Jun Lee <48055710+kkirico@users.noreply.github.com>

--- packages/server/src/entities/logic-entity/logic-entity.service.spec.ts
+++ packages/server/src/entities/logic-entity/logic-entity.service.spec.ts
@@ -1,101 +1,5 @@
-import { Logic } from '@models/logic/logic.schema';
+import { HintCalculatorEntity } from '@entities/hint-calculator-entity/hint-calculator-entity';
 import { LogicEntity } from './logic-entity.service';
-
-describe('정답 배열을 이용해서 힌트를 추출한다.', () => {
-  let logicEntity: LogicEntity;
-  beforeEach(() => {
-    const logicEntityDto = {
-      answer: [[]],
-      timeLimit: 0,
-      title: 'test',
-      authorId: 'kkirico',
-    };
-    logicEntity = new LogicEntity(logicEntityDto);
-  });
-
-  test('스파스 로직 케이스', () => {
-    const logic = [
-      [true, false, true],
-      [false, true, false],
-      [true, false, true],
-    ];
-
-    const hintSolution = {
-      row: [[1, 1], [1], [1, 1]],
-      column: [[1, 1], [1], [1, 1]],
-    };
-
-    const hints = logicEntity['getHints'](logic);
-    expect(hints).toEqual(hintSolution);
-  });
-
-  test('꽉 찬 로직 케이스', () => {
-    const logic = [
-      [true, true, true],
-      [true, true, true],
-      [true, true, true],
-    ];
-
-    const hintSolution = {
-      row: [[3], [3], [3]],
-      column: [[3], [3], [3]],
-    };
-
-    const hints = logicEntity['getHints'](logic);
-    expect(hints).toEqual(hintSolution);
-  });
-
-  test('그냥 로직 케이스', () => {
-    const logic = [
-      [true, false, true, false, true],
-      [true, true, false, false, true],
-      [true, true, true, true, false],
-      [true, true, false, true, true],
-      [true, true, true, true, false],
-    ];
-
-    const hintSolution = {
-      row: [[1, 1, 1], [2, 1], [4], [2, 2], [4]],
-      column: [[5], [4], [1, 1, 1], [3], [2, 1]],
-    };
-
-    const hints = logicEntity['getHints'](logic);
-    expect(hints).toEqual(hintSolution);
-  });
-});
-
-describe('로직의 한 열을 힌트 배열로 바꾸는 함수 테스트', () => {
-  let logicEntity: LogicEntity;
-  beforeEach(() => {
-    const logicEntityDto = {
-      answer: [[]],
-      timeLimit: 0,
-      title: 'test',
-      authorId: 'kkirico',
-    };
-    logicEntity = new LogicEntity(logicEntityDto);
-  });
-
-  test('일반 예시 테스트', () => {
-    const row = [true, true, true, false, true, false, true];
-    const hintOfRowSolution = [3, 1, 1];
-
-    const hintOfRow = logicEntity['splitSum'](row);
-
-    expect(hintOfRow).toEqual(hintOfRowSolution);
-  });
-
-  // 테스트 케이스에서 끝에 0
-  // 전체 0이면 버그를 찾을 수 있었다.
-  test('[0,1,0] 테스트', () => {
-    const row = [false, true, false];
-    const hintOfRowSolution = [1];
-
-    const hintOfRow = logicEntity['splitSum'](row);
-
-    expect(hintOfRow).toEqual(hintOfRowSolution);
-  });
-});
 
 describe('로직 생성 테스트', () => {
   test('일반적인 로직 생성', () => {
@@ -112,9 +16,8 @@ describe('로직 생성 테스트', () => {
     };
 
     const logic = new LogicEntity(logicEntity);
-    const { row: testHintRow, column: testHintColumn } = logic['getHints'](
-      logicEntity.answer,
-    );
+    const { row: testHintRow, column: testHintColumn } =
+      HintCalculatorEntity.getHints(logicEntity.answer);
     const testLogicEntity = {
       answer: [
         [true, false, true, true],
--- packages/server/src/entities/logic-entity/logic-entity.service.ts
+++ packages/server/src/entities/logic-entity/logic-entity.service.ts
@@ -1,5 +1,6 @@
-import { LogicEntityDto } from '@dto/logic/logic-entity.dto';
 import _ from 'lodash';
+import { LogicEntityDto } from '@dto/logic/logic-entity.dto';
+import { HintCalculatorEntity } from '@entities/hint-calculator-entity/hint-calculator-entity';
 
 export class LogicEntity {
   readonly answer: boolean[][];
@@ -15,35 +16,35 @@ export class LogicEntity {
     this.timeLimit = logicEntityDto.timeLimit;
     this.title = logicEntityDto.title;
     this.authorId = logicEntityDto.authorId;
-    this.hintRow = this.getHints(logicEntityDto.answer).row;
-    this.hintColumn = this.getHints(logicEntityDto.answer).column;
-    this.size = logicEntityDto.answer.length;
-  }
-
-  private splitSum(array: boolean[]): number[] {
-    return array
-      .map((cellState) => (cellState ? '1' : '0'))
-      .join('')
-      .split('0')
-      .reduce<number[]>((acc, cellState: string) => {
-        if (!cellState) return acc;
-        return [...acc, cellState.length];
-      }, []);
-  }
-
-  private getHints(answer: boolean[][]): {
-    row: number[][];
-    column: number[][];
-  } {
-    const rowsOfLogic = answer;
-    const columnsOfLogic = _.zip(...answer);
-
-    const rowHints = rowsOfLogic.map((row) => this.splitSum(row));
-    const columnHints = columnsOfLogic.map((column) => this.splitSum(column));
-
-    return {
-      row: rowHints,
-      column: columnHints,
-    };
+    this.hintRow = logicEntityDto.answer
+      ? HintCalculatorEntity.getHints(logicEntityDto.answer).row
+      : undefined;
+    this.hintColumn = logicEntityDto.answer
+      ? HintCalculatorEntity.getHints(logicEntityDto.answer).column
+      : undefined;
+    this.size = logicEntityDto.answer
+      ? logicEntityDto.answer.length
+      : undefined;
   }
 }
+
+//문제: 로직엔티티에 있는 프로퍼티들이 업데이트할 때는 전부 존재하지 않음
+
+// 1. 로직 엔티티가 비어있는 상태로 만들어지는건 위험하다.
+// 비어있는 상태가 괜찮으려면?
+//  - 업데이트를 위한 엔티티여야 한다.
+//    X 업데이트를 위한 엔티티를 따로 만드나?
+//  - id로 로직을 식별.
+//    - id를 앱단에서 생성해야됨.
+//  - 비어있어도 되는 경우와 안되는 경우를 분리.
+//      - logicEntity 내에 스테이트를 가지거나
+//      X logicEntity를 두 개로 분리해야 됨.
+//        - 로직엔티티가 자체적으로 완성본이어야 하지 않나?
+// 2. getHints가 엔티티 내에 있어야 되는가?
+//  - 맞는거 같음
+
+// updateDto._id -> db에서 로직을 가져와서 -> 가져온 로직을 엔티티에 넣어서 수정하고 -> db 업데이트
+//  - 불필요하게 로직을 가져와야함 - 이건 진짜 개 이상한거임(db에서 할 일을 코드에서함)
+
+// 로직 분리 시
+// (partial)LogicEntity 에 넣어서 -> db 업데이트
