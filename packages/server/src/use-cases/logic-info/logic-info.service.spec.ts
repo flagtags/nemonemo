9821e89ccffcc444863b35f60ee64d5997b229c7
Dante <48055710+kkirico@users.noreply.github.com>
2023-03-19 17:23:26 +0900

    유즈케이스- 좋아요 기록 추가 #9
    Co-Authored-By: 박재윤 <j03y14@naver.com>

--- packages/server/src/use-cases/logic-info/logic-info.service.spec.ts
+++ packages/server/src/use-cases/logic-info/logic-info.service.spec.ts
@@ -1,14 +1,17 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { LogicInfoModel } from '@models/logicInfo/logicInfo.model';
-import { LogicInfoService } from './logic-info.service';
 import { UpdateLogicInfoDto } from '@dto/logicInfo/update-logic-info.dto';
 import { LogicNotFoundError } from '@errors/logic';
+import { LikesHistoryModel } from '@models/likesHisory/likesHistory.model';
+import { LogicInfoModel } from '@models/logicInfo/logicInfo.model';
+import { Test, TestingModule } from '@nestjs/testing';
+import { LogicInfoService } from './logic-info.service';
 
 jest.mock('@models/logicInfo/logicInfo.model');
+jest.mock('@models/likesHistory/likesHistory.model');
 
 describe('Logic Info Service', () => {
   let logicInfoService: LogicInfoService;
   let mocekdLogicInfoModel: jest.Mocked<LogicInfoModel>;
+  let mockedLikesHistoryModel: jest.Mocked<LikesHistoryModel>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -17,7 +20,9 @@ describe('Logic Info Service', () => {
 
     logicInfoService = module.get<LogicInfoService>(LogicInfoService);
     const logicInfoModel = module.get<LogicInfoModel>(LogicInfoModel);
+    const likesHistoryModel = module.get<LikesHistoryModel>(LikesHistoryModel);
     mocekdLogicInfoModel = jest.mocked(logicInfoModel, true);
+    mockedLikesHistoryModel = jest.mocked(likesHistoryModel, true);
   });
 
   describe('Views increment 1', () => {
@@ -53,20 +58,43 @@ describe('Logic Info Service', () => {
     });
   });
 
-  describe('likes increment 1', () => {
-    test('likes increment 1', async () => {
+  describe('likes toggle', () => {
+    test('likes increment 1: 클릭 이력이 없는 경우', async () => {
+      const hasHistory = mockedLikesHistoryModel.hasOne.mockResolvedValue({
+        response: false,
+      });
+
       const updateLogicInfoDto: UpdateLogicInfoDto = {
         logicId: 'asdf',
       };
 
-      mocekdLogicInfoModel.increaseLikes.mockResolvedValue({
-        response: 1,
-        matched: 1,
-      });
-
-      await logicInfoService.increaseLikes(updateLogicInfoDto.logicId);
+      await logicInfoService.toggleLikes(updateLogicInfoDto.logicId);
 
       expect(mocekdLogicInfoModel.increaseLikes).toHaveBeenCalledWith(
+        updateLogicInfoDto,
+      );
+
+      expect(mocekdLogicInfoModel.decreaseLikes).not.toHaveBeenCalledWith(
+        updateLogicInfoDto,
+      );
+    });
+
+    test('likes decrement 1: 클릭 이력이 있는 경우', async () => {
+      const hasHistory = mockedLikesHistoryModel.hasOne.mockResolvedValue({
+        response: false,
+      });
+
+      const updateLogicInfoDto: UpdateLogicInfoDto = {
+        logicId: 'asdf',
+      };
+
+      await logicInfoService.toggleLikes(updateLogicInfoDto.logicId);
+
+      expect(mocekdLogicInfoModel.increaseLikes).not.toHaveBeenCalledWith(
+        updateLogicInfoDto,
+      );
+
+      expect(mocekdLogicInfoModel.decreaseLikes).toHaveBeenCalledWith(
         updateLogicInfoDto,
       );
     });
@@ -82,7 +110,7 @@ describe('Logic Info Service', () => {
       });
 
       await expect(async () =>
-        logicInfoService.increaseLikes(updateLogicInfoDto.logicId),
+        logicInfoService.toggleLikes(updateLogicInfoDto.logicId),
       ).rejects.toThrowError(new LogicNotFoundError());
     });
   });
--- packages/server/src/use-cases/logic-info/logic-info.service.ts
+++ packages/server/src/use-cases/logic-info/logic-info.service.ts
@@ -2,10 +2,18 @@ import { Injectable } from '@nestjs/common';
 import { LogicInfoModel } from '@models/logicInfo/logicInfo.model';
 import { UpdateLogicInfoDto } from '@dto/logicInfo/update-logic-info.dto';
 import { LogicNotFoundError } from '@errors/logic';
+import { LikesHistoryModel } from '@models/likesHisory/likesHistory.model';
+import { LikeDto } from '@dto/logicInfo/like-dto';
+import { TransactionPlugin } from '@models/transactionPlugin';
+import { IModelResponse } from '@models/response';
 
 @Injectable()
 export class LogicInfoService {
-  constructor(private readonly logicInfoModel: LogicInfoModel) {}
+  constructor(
+    private readonly logicInfoModel: LogicInfoModel,
+    private readonly likesHistoryModel: LikesHistoryModel,
+    private readonly transactionPlugin: TransactionPlugin,
+  ) {}
 
   async increaseViews(logicId: UpdateLogicInfoDto['logicId']) {
     const { response, matched } = await this.logicInfoModel.increaseViews({
@@ -17,9 +25,23 @@ export class LogicInfoService {
     return response;
   }
 
-  async increaseLikes(logicId: UpdateLogicInfoDto['logicId']) {
-    const { response, matched } = await this.logicInfoModel.increaseLikes({
-      logicId,
+  async toggleLikes({ logicId, userId }: LikeDto) {
+    const { matched: likeHistoryMatched } = await this.likesHistoryModel.hasOne(
+      {
+        logicId,
+        userId,
+      },
+    );
+    const { response, matched } = this.transactionPlugin.execute(async () => {
+      if (likeHistoryMatched)
+        await this.likesHistoryModel.deleteLike({ logicId, userId });
+      else await this.likesHistoryModel.createLike({ logicId, userId });
+
+      const calculateLikes = likeHistoryMatched
+        ? this.logicInfoModel.decreaseLikes
+        : this.logicInfoModel.increaseLikes;
+
+      return await calculateLikes({ logicId });
     });
 
     if (!matched) throw new LogicNotFoundError();
