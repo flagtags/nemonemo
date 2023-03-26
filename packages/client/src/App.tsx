42404739e76c63a276dc268a146aa847d476002f
Dante <48055710+kkirico@users.noreply.github.com>
2023-03-26 17:49:47 +0900

    라우터 적용 #17
    Co-Authored-By: 박재윤 <j03y14@naver.com>

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,12 +1,8 @@
-import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import styled from 'styled-components';
-import { CELL_STATE } from './components/LogicPaper/type';
-import Cell from './components/LogicPaper/Cell';
-import Header from './components/Header';
 import getLogic from './api/getLogic';
 import './App.css';
-import LogicPaper from './components/LogicPaper';
-import _ from 'lodash';
+import Login from './components/Account/Login';
 
 const Container = styled.div`
   display: flex;
@@ -15,12 +11,20 @@ const Container = styled.div`
 
 function App() {
   const logic = getLogic();
-
   return (
-    <Container className="nemonemologic">
-      <Header title={logic.title} />
-      <LogicPaper rowLength={10} colLength={10} solution={logic.solution} />
-    </Container>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={<></>}
+        />
+
+        <Route
+          path="/account"
+          element={<Login />}
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ packages/client/src/pages/game/index.tsx
@@ -0,0 +1,27 @@
+import styled from 'styled-components';
+import getLogic from '../../api/getLogic';
+import Header from '../../components/Header';
+import LogicPaper from '../../components/LogicPaper';
+import './App.css';
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+function App() {
+  const logic = getLogic();
+
+  return (
+    <Container className="nemonemologic">
+      <Header title={logic.title} />
+      <LogicPaper
+        rowLength={10}
+        colLength={10}
+        solution={logic.solution}
+      />
+    </Container>
+  );
+}
+
+export default App;
