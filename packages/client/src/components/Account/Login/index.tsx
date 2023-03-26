87aec3592ef7d71d4efe25314236548288a15322
Dante <48055710+kkirico@users.noreply.github.com>
2023-03-26 16:09:13 +0900

    로그인 이벤트 추가 #17
    Co-Authored-By: 박재윤 <j03y14@naver.com>

--- packages/client/src/components/Account/Login/index.tsx
+++ packages/client/src/components/Account/Login/index.tsx
@@ -1,12 +1,41 @@
-import React from 'react';
+import { useState } from 'react';
+import { redirect } from 'react-router-dom';
+import Fetcher from '../../../api/fetcher';
 
 const Login = () => {
+  const [userName, setUserName] = useState('');
+  const [password, setPassword] = useState('');
+
+  const onLoginPress = () => {
+    const loginFetcher = new Fetcher('/user/login');
+
+    loginFetcher
+      .post({
+        userName,
+        password,
+      })
+      .then(() => {
+        redirect('/');
+      })
+      .catch((error) => {
+        window.alert('로그인 실패!!');
+      });
+  };
+
   return (
     <>
       <h1>로그인</h1>
-      <input placeholder="아이디" />
-      <input placeholder="비밀번호 " />
-      <button>로그인</button>
+      <input
+        placeholder="아이디"
+        onChange={(e) => setUserName(e.target.value)}
+        value={userName}
+      />
+      <input
+        placeholder="비밀번호 "
+        onChange={(e) => setPassword(e.target.value)}
+        value={password}
+      />
+      <button onClick={onLoginPress}>로그인</button>
     </>
   );
 };
