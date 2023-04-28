f217e1f7cd105fff2d308a01f8dc997e5fa3b6a9
Dante <48055710+kkirico@users.noreply.github.com>
2023-04-28 23:04:30 +0900

    fetcher 수정 #20
    Co-Authored-By: 박재윤 <j03y14@naver.com>

--- packages/client/src/api/fetcher.ts
+++ packages/client/src/api/fetcher.ts
@@ -13,48 +13,48 @@ export default class Fetcher {
     this.query = {};
   }
 
-  setQuery(query: Query) {
+  setQuery = (query: Query) => {
     this.query = query;
     return this;
-  }
+  };
 
   get url() {
     return `${this.host}${this.path}`;
   }
 
-  async get<T>() {
+  get = async <T>() => {
     const { data } = await axios.get<T>(this.url, {
       params: this.query,
       withCredentials: true,
     });
 
     return data;
-  }
+  };
 
-  async post<T>(body: object) {
+  post = async <T>(body: object) => {
     const { data } = await axios.post<T>(this.url, body, {
       params: this.query,
       withCredentials: true,
     });
 
     return data;
-  }
+  };
 
-  async patch<T>(body: object) {
+  patch = async <T>(body: object) => {
     const { data } = await axios.patch<T>(this.url, body, {
       params: this.query,
       withCredentials: true,
     });
 
     return data;
-  }
+  };
 
-  async delete<T>() {
+  delete = async <T>() => {
     const { data } = await axios.delete<T>(this.url, {
       params: this.query,
       withCredentials: true,
     });
 
     return data;
-  }
+  };
 }
