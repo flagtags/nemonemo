b235a1e929032025378f499387997827b785f0f5
Dante <48055710+kkirico@users.noreply.github.com>
2023-08-02 22:13:06 +0900

    로직 보드 스토어 추가 #22
    Co-Authored-By: 박재윤 <j03y14@naver.com>

--- packages/client/src/hooks/useLogicBoard.ts
+++ packages/client/src/hooks/useLogicBoard.ts
@@ -1,6 +1,10 @@
+import LogicBoardStore, {
+  logicBoardStoreSnapshot,
+} from '@/store/logicBoardStore';
 import { CELL_STATE } from '@/types/logic';
 import _ from 'lodash';
-import { useEffect, useReducer } from 'react';
+import { useEffect, useReducer, useSyncExternalStore } from 'react';
+
 export interface IChangeCellStateAction {
   init: boolean;
   rowLength: number;
@@ -16,58 +20,28 @@ export interface IChangeCellState {
   toBe?: CELL_STATE;
 }
 
+const logicBoardStore = new LogicBoardStore();
+
 const useLogicBoard = ({
   rowLength,
   colLength,
 }: {
   rowLength: number;
   colLength: number;
-}) => {
-  const cellStateReducer = (
-    state: CELL_STATE[][],
-    action: IChangeCellStateAction,
-  ): CELL_STATE[][] => {
-    const tempCellStates = _.cloneDeep(state);
-
-    if (action.init === true) {
-      return Array(action.rowLength)
-        .fill(0)
-        .map(() => Array(action.colLength).fill(CELL_STATE.BLANK));
-    }
-
-    if (action.rowIndex === undefined || action.colIndex === undefined)
-      return tempCellStates;
-
-    switch (action.toBe) {
-      case CELL_STATE.FILL:
-        tempCellStates[action.rowIndex][action.colIndex] = CELL_STATE.FILL;
-        return tempCellStates;
-      case CELL_STATE.BLANK:
-        tempCellStates[action.rowIndex][action.colIndex] = CELL_STATE.BLANK;
-        return tempCellStates;
-      case CELL_STATE.NOTHING:
-        tempCellStates[action.rowIndex][action.colIndex] = CELL_STATE.NOTHING;
-        return tempCellStates;
-      default:
-        return state;
-    }
-  };
-
-  const [cellStates, changeCellState] = useReducer(
-    cellStateReducer,
-    Array(rowLength)
-      .fill(0)
-      .map(() => Array(colLength).fill(CELL_STATE.BLANK)),
+}): [logicBoardStoreSnapshot, LogicBoardStore] => {
+  const snapshot = useSyncExternalStore(
+    (onStoreChange) => {
+      logicBoardStore.addListener(onStoreChange);
+      return () => logicBoardStore.removeListener(onStoreChange);
+    },
+    () => logicBoardStore.getSnapshot(),
   );
 
   useEffect(() => {
-    changeCellState({ init: true, rowLength, colLength });
-  }, [rowLength, colLength]);
+    logicBoardStore.initialize(rowLength);
+  }, [rowLength]);
 
-  return {
-    cellStates,
-    changeCellState: changeCellState as React.Dispatch<IChangeCellState>,
-  };
+  return [snapshot, logicBoardStore];
 };
 
 export default useLogicBoard;
