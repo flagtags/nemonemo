6ffd0df94b5dce3368710d33fb8132c269b1fdfa
Dante <48055710+kkirico@users.noreply.github.com>
2023-09-06 22:15:56 +0900

    타이머 리팩토링 #25
    타이머 뷰 컴포넌트
    타이머 로직을 useTimer 훅으로 빼냄
    
    Co-Authored-By: 박재윤 <j03y14@naver.com>

--- packages/client/src/pages/game/index.tsx
+++ packages/client/src/pages/game/index.tsx
@@ -7,9 +7,11 @@ import { useParams } from 'react-router-dom';
 import { ILogic } from '@/types/logic';
 import { useQuery } from 'react-query';
 import options from '@/config/reactQuery/options';
-import { useMemo } from 'react';
+import { useMemo, useState } from 'react';
 import { time } from 'console';
 import Timer from './Timer';
+import Popup from '@/components/Popup';
+import useTimer from '@/hooks/useTimer';
 
 const Container = styled.div`
   display: flex;
@@ -18,11 +20,30 @@ const Container = styled.div`
   padding: 16px;
 `;
 
+const PopupContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 8px;
+`;
+const PopupButton = styled.button`
+  border: 1px solid black;
+  padding-top: 8px;
+  padding-left: 16px;
+  padding-right: 16px;
+  padding-bottom: 8px;
+`;
+
+const Spacer = styled.div`
+  width: 16px;
+  height: 16px;
+`;
+
 const convertSolution = (solution: boolean[][]) =>
   solution.map((row) => row.map((col) => (col ? 1 : 0)));
 
 function Game() {
   const { logicId } = useParams();
+  const [isPopupOpen, setIsPopupOpen] = useState(false);
   if (!logicId) throw new Error('logic id is not defined');
 
   const { data: logic } = useQuery(
@@ -35,9 +56,26 @@ function Game() {
 
   const { title, answer, size, timeLimit, authorId } = logic;
 
-  const numberSolution = useMemo(() => convertSolution(answer), [answer]);
+  const onTimerEnd = () => {
+    setIsPopupOpen(true);
+  };
 
-  const initGame = () => null;
+  const { remainSeconds, startTimer, isCounting, init, stopTimer } = useTimer(
+    timeLimit,
+    onTimerEnd,
+  );
+
+  const onContinue = () => {
+    setIsPopupOpen(false);
+  };
+
+  const onRestart = () => {
+    init();
+    startTimer();
+    setIsPopupOpen(false);
+  };
+
+  const numberSolution = useMemo(() => convertSolution(answer), [answer]);
 
   return (
     <Container className="nemonemologic">
@@ -45,14 +83,28 @@ function Game() {
       <h6> author: {authorId} </h6>
 
       <Timer
-        timeLimit={timeLimit}
-        initGame={initGame}
+        remainTime={remainSeconds}
+        startTimer={startTimer}
+        isCounting={isCounting}
       />
+
+      <Popup
+        isOpen={isPopupOpen}
+        close={() => {}}
+      >
+        <PopupContainer>
+          <h2>시간 종료!</h2>
+          <PopupButton onClick={onContinue}>계속하기</PopupButton>
+          <Spacer />
+          <PopupButton onClick={onRestart}>다시하기</PopupButton>
+        </PopupContainer>
+      </Popup>
 
       <LogicPaper
         rowLength={size}
         colLength={size}
         solution={numberSolution}
+        onEnd={stopTimer}
       />
     </Container>
   );
